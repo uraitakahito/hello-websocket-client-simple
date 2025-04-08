0756c3014589730dfa5356fa962918fd6cf2f391
Takahito Urai <uraitakahito@gmail.com>
2025-04-08 20:10:41 +0900

    Add README and main.rb for websocket client implementation

---
+++ src/main.rb
@@ -0,0 +1,24 @@
+require 'websocket-client-simple'
+
+ws = WebSocket::Client::Simple.connect 'wss://ws.ifelse.io/'
+
+ws.on :message do |msg|
+  puts msg.data
+end
+
+ws.on :open do
+  ws.send 'hello!!!'
+end
+
+ws.on :close do |e|
+  p e
+  exit 1
+end
+
+ws.on :error do |e|
+  p e
+end
+
+loop do
+  ws.send STDIN.gets.strip
+end
